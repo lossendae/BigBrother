89d2b0e489ee8750a5d188cd0404790fc5ba6fb7
lossendae <lossendae@gmail.com>
2011-11-11 01:21:46 +0100

    Added cache_timeout system setting

--- _build/data/transport.settings.php
+++ _build/data/transport.settings.php
@@ -17,4 +17,13 @@ $settings['bigbrother.is_authenticated']->fromArray(array(
     'area' => 'Google Analytics for MODx Revolution',
 ),'',true,true);
 
+$settings['bigbrother.cache_timeout']= $modx->newObject('modSystemSetting');
+$settings['bigbrother.cache_timeout']->fromArray(array(
+    'key' => 'bigbrother.cache_timeout',
+    'value' => 300,
+    'xtype' => 'textfield',
+    'namespace' => 'bigbrother',
+    'area' => 'Google Analytics for MODx Revolution',
+),'',true,true);
+
 return $settings;
