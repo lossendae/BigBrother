05a24a74a212b204ce26fe8c708b9d628d694808
lossendae <lossendae@gmail.com>
2011-12-07 17:03:15 +0100

    Removed unneeded system setting

--- _build/data/transport.settings.php
+++ _build/data/transport.settings.php
@@ -8,15 +8,6 @@
 
 $settings = array();
 
-$settings['bigbrother.is_authenticated']= $modx->newObject('modSystemSetting');
-$settings['bigbrother.is_authenticated']->fromArray(array(
-    'key' => 'bigbrother.is_authenticated',
-    'value' => false,
-    'xtype' => 'combo-boolean',
-    'namespace' => 'bigbrother',
-    'area' => 'Google Analytics for MODx Revolution',
-),'',true,true);
-
 $settings['bigbrother.cache_timeout']= $modx->newObject('modSystemSetting');
 $settings['bigbrother.cache_timeout']->fromArray(array(
     'key' => 'bigbrother.cache_timeout',
