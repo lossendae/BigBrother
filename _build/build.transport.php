eb1ea8db948a5e842c45c95994608899b2e5b669
lossendae <lossendae@gmail.com>
2012-03-09 11:32:07 +0100

    Update version before release

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -14,7 +14,7 @@ set_time_limit(0);
 /* define package */
 define('PKG_NAME','BigBrother');
 define('PKG_NAMESPACE','bigbrother');
-define('PKG_VERSION','1.0');
+define('PKG_VERSION','1.0.1');
 define('PKG_RELEASE','pl');
 
 function getSnippetContent($path, $name, $debug = false) {
