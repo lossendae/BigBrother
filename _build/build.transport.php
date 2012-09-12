fc0265b83c38dbd4074666c13f4c30101ae10875
lossendae <lossendae@gmail.com>
2012-09-12 21:23:21 +0200

    Update version and changelog

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -14,8 +14,8 @@ set_time_limit(0);
 /* define package */
 define('PKG_NAME','BigBrother');
 define('PKG_NAMESPACE','bigbrother');
-define('PKG_VERSION','1.1.2');
-define('PKG_RELEASE','rc1');
+define('PKG_VERSION','1.1.0');
+define('PKG_RELEASE','rc2');
 
 function getSnippetContent($path, $name, $debug = false) {
     $name = ($debug) ? 'debug.'. $name .'.php' : $name .'.php';
